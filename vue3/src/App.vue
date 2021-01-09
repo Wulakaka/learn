316b0e2aaa832f6373f6f9e2b5f3927adb36a059
weifeng.zhang <34675974+Wulakaka@users.noreply.github.com>
2021-01-09 23:00:07 +0800

    add vue3 file

---
+++ vue3/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <!--  <img alt="Vue logo" src="./assets/logo.png">-->
+  <!--  <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+  <Draw></Draw>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import Draw from './components/Draw.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Draw
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+//color: #2c3e50;
+//margin-top: 60px;
+}
+</style>
