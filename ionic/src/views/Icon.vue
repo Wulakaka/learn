80151afa863d47fcac9179b617ce53d61bd0881f
weifeng.zhang <wulakaka@live.com>
2021-03-13 18:26:54 +0800

    增加ionic相关文件

---
+++ ionic/src/views/Icon.vue
@@ -0,0 +1,37 @@
+<template>
+<ion-page>
+  <ion-content>
+    <ion-icon :icon="heart"></ion-icon>
+    <ion-icon :ios="logoApple" :md="logoAndroid"></ion-icon>
+  </ion-content>
+</ion-page>
+</template>
+
+<script>
+import {heart,logoApple, logoAndroid} from 'ionicons/icons'
+import {
+  IonPage,
+  IonContent,
+  IonIcon
+} from '@ionic/vue'
+import {defineComponent} from "vue"
+export default defineComponent({
+  name: "Icon",
+  components: {
+    IonContent,
+    IonPage,
+    IonIcon
+  },
+  setup() {
+    return {
+      heart,
+      logoApple,
+      logoAndroid
+    }
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
