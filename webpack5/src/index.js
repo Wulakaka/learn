c862f70c8194ba5c11f52286dd19c96541d632cf
Wulakaka <wulakaka@live.com>
2021-06-26 23:15:00 +0800

    回退代码

--- webpack5/src/index.js
+++ webpack5/src/index.js
@@ -1,17 +1,9 @@
 import _ from 'lodash'
-import './style.css'
-import Icon from './icon.jpg'
-import Data from './data.xml'
+
 function component() {
     const element = document.createElement('div')
     element.innerHTML = _.join(['Hello', 'webpack'], ' ')
-    element.classList.add('hello')
 
-    var myIcon = new Image()
-    myIcon.src = Icon
-    element.appendChild(myIcon)
-
-    console.log(Data)
     return element
 }
 document.body.appendChild(component())
--- webpack5/webpack.config.js
+++ webpack5/webpack.config.js
@@ -5,34 +5,5 @@ module.exports = {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist')
     },
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: [
-                    'style-loader',
-                    'css-loader'
-                ]
-            },
-            {
-                test: /\.(png|svg|jpg|gif)$/,
-                use: [
-                    'file-loader'
-                ]
-            },
-            {
-                test: /\.(csv|tsv)$/,
-                use: [
-                    'csv-loader'
-                ]
-            },
-            {
-                test: /\.xml$/,
-                use:[
-                    'xml-loader'
-                ]
-            }
-        ]
-    },
     mode: 'production'
 }
