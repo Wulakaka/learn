144a0f28eeb54226ce2d7b1ffee7a8eb5ea6d3b3
Wulakaka <wulakaka@live.com>
2021-06-26 22:53:08 +0800

    增加样式文件

--- webpack5/webpack.config.js
+++ webpack5/webpack.config.js
@@ -5,5 +5,16 @@ module.exports = {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist')
     },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            }
+        ]
+    },
     mode: 'production'
 }
