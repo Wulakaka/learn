c8c7a6658bf6716d124a0666389be33d86cc122c
Wulakaka <wulakaka@live.com>
2021-06-26 23:01:57 +0800

    增加图片

--- webpack5/src/index.js
+++ webpack5/src/index.js
@@ -1,9 +1,14 @@
 import _ from 'lodash'
 import './style.css'
+import Icon from './icon.jpg'
 function component() {
     const element = document.createElement('div')
     element.innerHTML = _.join(['Hello', 'webpack'], ' ')
     element.classList.add('hello')
+
+    var myIcon = new Image()
+    myIcon.src = Icon
+    element.appendChild(myIcon)
     return element
 }
 document.body.appendChild(component())
