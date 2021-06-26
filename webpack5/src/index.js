fd5ed16282903ace15fee54d76a39a467b9fc401
Wulakaka <wulakaka@live.com>
2021-06-26 23:12:16 +0800

    增加xml文件

--- webpack5/src/index.js
+++ webpack5/src/index.js
@@ -1,6 +1,7 @@
 import _ from 'lodash'
 import './style.css'
 import Icon from './icon.jpg'
+import Data from './data.xml'
 function component() {
     const element = document.createElement('div')
     element.innerHTML = _.join(['Hello', 'webpack'], ' ')
@@ -9,6 +10,8 @@ function component() {
     var myIcon = new Image()
     myIcon.src = Icon
     element.appendChild(myIcon)
+
+    console.log(Data)
     return element
 }
 document.body.appendChild(component())
