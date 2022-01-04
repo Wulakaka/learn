e0904da95d9de422341c2be43f859896b3173fc7
weifeng.zhang <34675974+Wulakaka@users.noreply.github.com>
2022-01-04 23:55:03 +0800

    feat: add mocha and test files

---
+++ webpack5-threejs/test/removeDuplicate.mjs
@@ -0,0 +1,14 @@
+// import removeDuplicates from "../src/leetcode/removeDuplicates";
+import {add} from "../src/add.mjs";
+import assert from 'assert';
+// describe('删除重复元素', function () {
+//     describe('#removeDuplicates()', function () {
+//         it('should return 3 when the value is [1,1,2,3,3]', function () {
+//             assert.equal(removeDuplicates([1,1,2,3,3]), 3)
+//         });
+//     })
+//
+// })
+it('should add to numbers from an es module', () => {
+    assert.equal(add(3, 5), 8);
+});
