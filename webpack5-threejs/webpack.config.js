d1746f20b2a770942de5aaca403747e36e4cd393
weifeng.zhang <34675974+Wulakaka@users.noreply.github.com>
2022-01-05 23:31:36 +0800

    style: 格式化代码

--- webpack5-threejs/webpack.config.js
+++ webpack5-threejs/webpack.config.js
@@ -1,29 +1,29 @@
-const path = require("path")
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    mode: "development", // 使用development 模式打包，不要在正式环境中使用
-    entry: './src/index.js',
-    devtool: "inline-source-map", // 使用source-map
-    devServer: {
-        static: './dist' // 将 dist 目录下的文件 serve 到 localhost:8080 下
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'Output Management',
-        })
+  mode: "development", // 使用development 模式打包，不要在正式环境中使用
+  entry: "./src/index.js",
+  devtool: "inline-source-map", // 使用source-map
+  devServer: {
+    static: "./dist", // 将 dist 目录下的文件 serve 到 localhost:8080 下
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Output Management",
+    }),
+  ],
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true, // 打包时清理目录
+  },
+  module: {
+    rules: [
+      {
+        test: /.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
     ],
-    output: {
-        filename: "[name].bundle.js",
-        path: path.resolve(__dirname, 'dist'),
-        clean: true  // 打包时清理目录
-    },
-    module: {
-        rules: [
-            {
-                test: /.css$/i,
-                use: ['style-loader', 'css-loader']
-            }
-        ]
-    }
-}
+  },
+};
