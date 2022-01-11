290fd2ba06126296511d51b9f75945716a881c1e
张伟峰 <zhang.weifeng@tslsmart.com>
2022-01-11 16:00:56 +0800

    feat: 增加颜色输出

--- node/src/app.js
+++ node/src/app.js
@@ -13,6 +13,29 @@ server.listen(port,hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`)
 })
 
+console.log('我的%s已经%d岁','猫',2)
+console.log('%o', Number)
+const oranges = ['橙子', '橙子']
+const apples = ['苹果']
+oranges.forEach(fruit => {
+    console.count(fruit)
+})
+apples.forEach(fruit => {
+    console.count(fruit)
+})
+
+const doSomething = () => console.log('测试')
+const measureDoingSomething = () => {
+    console.time('doSomething')
+    doSomething()
+    console.timeEnd('doSomething')
+}
+measureDoingSomething()
+
+console.log('\x1b[33m%s\x1b[0m','你好')
+const chalk = require('chalk')
+console.log(chalk.blue('测试'))
+
 // process.argv.forEach((val,index) => {
 //     console.log(`${index}:${val}`)
 // })
