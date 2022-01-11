32d465293a5cecbc99b5b163cc0513943aff6313
张伟峰 <zhang.weifeng@tslsmart.com>
2022-01-11 16:05:41 +0800

    feat: 增加进度条

--- node/src/app.js
+++ node/src/app.js
@@ -1,19 +1,19 @@
 const http = require('http')
 
-const hostname ='127.0.0.1';
+const hostname = '127.0.0.1';
 const port = 3000
 
-const server = http.createServer((req,res) => {
+const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain')
     res.end('Hello World')
 })
 
-server.listen(port,hostname, () => {
+server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`)
 })
 
-console.log('我的%s已经%d岁','猫',2)
+console.log('我的%s已经%d岁', '猫', 2)
 console.log('%o', Number)
 const oranges = ['橙子', '橙子']
 const apples = ['苹果']
@@ -32,7 +32,7 @@ const measureDoingSomething = () => {
 }
 measureDoingSomething()
 
-console.log('\x1b[33m%s\x1b[0m','你好')
+console.log('\x1b[33m%s\x1b[0m', '你好')
 const chalk = require('chalk')
 console.log(chalk.blue('测试'))
 
@@ -44,5 +44,15 @@ console.log(chalk.blue('测试'))
 // const args = require('minimist')(process.argv.slice(2))
 // console.log(args['name'] )//joe
 
-// 主动退出
-process.exit(1)
+
+const ProgressBar = require('progress')
+const bar = new ProgressBar(':bar', {total: 10})
+const timer = setInterval(() => {
+    bar.tick()
+    if (bar.complete) {
+        clearInterval(timer)
+        // 主动退出
+        process.exit(1)
+    }
+}, 100)
+
