7e5dc6b1ffa9698226c4cf1fa2b938c0830b299e
张伟峰 <zhang.weifeng@tslsmart.com>
2022-01-11 15:19:02 +0800

    feat: 注释代码

--- node/src/app.js
+++ node/src/app.js
@@ -13,13 +13,13 @@ server.listen(port,hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`)
 })
 
-process.argv.forEach((val,index) => {
-    console.log(`${index}:${val}`)
-})
+// process.argv.forEach((val,index) => {
+//     console.log(`${index}:${val}`)
+// })
 
 // 用于截取参数，但是参数形式必须为--name=value
-const args = require('minimist')(process.argv.slice(2))
-console.log(args['name'] )//joe
+// const args = require('minimist')(process.argv.slice(2))
+// console.log(args['name'] )//joe
 
 // 主动退出
 process.exit(1)
