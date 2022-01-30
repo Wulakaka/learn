ea0d3da2610d2724fc4e25bea699967a212cb559
张伟峰 <zhang.weifeng@tslsmart.com>
2022-01-30 10:46:35 +0800

    fix: 处理eslint报错

--- vue3/src/components/Draw.vue
+++ vue3/src/components/Draw.vue
@@ -3,25 +3,26 @@
 </template>
 
 <script>
-import { onMounted, ref } from 'vue'
+import {onMounted, ref} from 'vue'
 import lineData from '@/assets/data'
+
 export default {
-  name: 'Draw',
-  setup () {
+  name: 'CanvasDraw',
+  setup() {
     const draw = ref(null)
     console.log(lineData.length)
     const scaleX = 10 / 200 * 10
     const scaleY = 10 / 0.5 * 10
-    const list = lineData.map(({ x, y }) => ({ x: x * scaleX * 1000, y: scaleY - y * scaleY }))
-    const list2 = lineData.map(({ x, y }) => ({ x: x * scaleX * 1000, y: scaleY * 2 - y * scaleY }))
+    const list = lineData.map(({x, y}) => ({x: x * scaleX * 1000, y: scaleY - y * scaleY}))
+    const list2 = lineData.map(({x, y}) => ({x: x * scaleX * 1000, y: scaleY * 2 - y * scaleY}))
       .filter((item, i) => {
         return i % 2 === 0
       })
-    const list3 = lineData.map(({ x, y }) => ({ x: x * scaleX * 1000, y: scaleY * 3 - y * scaleY }))
+    const list3 = lineData.map(({x, y}) => ({x: x * scaleX * 1000, y: scaleY * 3 - y * scaleY}))
       .filter((item, i) => {
         return i % 3 === 0
       })
-    const list4 = lineData.map(({ x, y }) => ({ x: x * scaleX * 1000, y: scaleY * 4 - y * scaleY }))
+    const list4 = lineData.map(({x, y}) => ({x: x * scaleX * 1000, y: scaleY * 4 - y * scaleY}))
       .filter((item, i) => {
         return i % 5 === 0
       })
@@ -30,28 +31,28 @@ export default {
       ctx.strokeStyle = 'red'
       ctx.beginPath()
       ctx.moveTo(0, scaleY)
-      list.forEach(({ x, y }) => {
+      list.forEach(({x, y}) => {
         ctx.lineTo(x, y)
       })
       ctx.stroke()
       ctx.strokeStyle = 'red'
       ctx.beginPath()
       ctx.moveTo(0, scaleY * 2)
-      list2.forEach(({ x, y }) => {
+      list2.forEach(({x, y}) => {
         ctx.lineTo(x, y)
       })
       ctx.stroke()
       ctx.strokeStyle = 'red'
       ctx.beginPath()
       ctx.moveTo(0, scaleY * 3)
-      list3.forEach(({ x, y }) => {
+      list3.forEach(({x, y}) => {
         ctx.lineTo(x, y)
       })
       ctx.stroke()
       ctx.strokeStyle = 'red'
       ctx.beginPath()
       ctx.moveTo(0, scaleY * 4)
-      list4.forEach(({ x, y }) => {
+      list4.forEach(({x, y}) => {
         ctx.lineTo(x, y)
       })
       ctx.stroke()
