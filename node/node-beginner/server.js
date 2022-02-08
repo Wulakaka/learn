6cc3564027dd8e5d7e90245d1424f48c5753d6dc
张伟峰 <zhang.weifeng@tslsmart.com>
2022-02-08 15:24:33 +0800

    feat: 增加处理POST请求的请求处理函数

--- node/node-beginner/server.js
+++ node/node-beginner/server.js
@@ -3,13 +3,19 @@ const url = require('url')
 
 function start(route, handle) {
   function onRequest(request, response) {
+    let postData = ''
     const pathname = url.parse(request.url).pathname
     console.log(`Request for ${pathname} received.`)
 
-    response.writeHead(200, {
-      "Content-Type": 'text/plain'
+    request.setEncoding('utf8')
+    request.addListener('data', function (postDataChunk) {
+      postData += postDataChunk
+      console.log(`Received POST data chunk ${postDataChunk} .`)
     })
-    route(handle, pathname, response)
+
+    request.addListener('end', function () {
+      route(handle, pathname, response, postData)
+    })
   }
 
   http.createServer(onRequest).listen(8888)
