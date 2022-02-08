a3fd6a6c245615c74230baf7c42070ec57b9af3a
张伟峰 <zhang.weifeng@tslsmart.com>
2022-02-08 16:39:30 +0800

    feat: 增加POST图片功能

--- node/node-beginner/requestHandlers.js
+++ node/node-beginner/requestHandlers.js
@@ -1,17 +1,20 @@
-const querystring = require('querystring')
-
-function start(response, postData) {
+const fs = require('fs')
+const formidable = require('formidable')
+const path = require("path");
+const uploadImgPath = path.resolve('./tmp/test.png')
+console.log(uploadImgPath)
+function start(response) {
   console.log("Request handler 'start' was called.")
 
   const body = '<html>'+
-    '<head>'+
+    '<head title="学习Node.js">'+
     '<meta http-equiv="Content-Type" content="text/html; '+
     'charset=UTF-8" />'+
     '</head>'+
     '<body>'+
-    '<form action="/upload" method="post">'+
-    '<textarea name="text" rows="20" cols="60"></textarea>'+
-    '<input type="submit" value="Submit text" />'+
+    '<form action="/upload" enctype="multipart/form-data" method="post">'+
+    '<input type="file" name="upload">'+
+    '<input type="submit" value="Upload file" />'+
     '</form>'+
     '</body>'+
     '</html>';
@@ -21,12 +24,37 @@ function start(response, postData) {
   response.end();
 }
 
-function upload(response, postData) {
+function upload(response, request) {
   console.log("Request handler 'upload' was called.")
-  response.writeHead(200, {"Content-Type": 'text/plain'})
-  response.write("You've sent the text:" + querystring.parse(postData).text)
-  response.end()
+
+  const form = new formidable.IncomingForm()
+  console.log('about to parse')
+  form.parse(request, function(error, fields, files){
+    console.log('parsing done')
+    fs.copyFileSync(files.upload.filepath, uploadImgPath)
+    fs.unlinkSync(files.upload.filepath)
+    response.writeHead(200, {"Content-Type": 'text/html'})
+    response.write('received image: <br/>')
+    response.write('<img src="/show" alt="" />')
+    response.end()
+  })
+}
+
+function show (response) {
+  console.log("Request handler 'show' was called.")
+  fs.readFile(uploadImgPath, 'binary', function (error, file) {
+    if (error) {
+      response.writeHead(500, {'Content-Type': 'text/plain'})
+      response.write(error + '\n')
+      response.end()
+    } else {
+      response.writeHead(200, {'Content-Type': 'image/png'})
+      response.write(file, 'binary')
+      response.end()
+    }
+  })
 }
 
 exports.start = start
 exports.upload = upload
+exports.show = show
