4696a5f06d763e72012511ce81d230a5f563c66d
张伟峰 <zhang.weifeng@tslsmart.com>
2022-02-08 15:04:06 +0800

    feat: 添加良好的请求处理程序

--- node/node-beginner/server.js
+++ node/node-beginner/server.js
@@ -9,9 +9,7 @@ function start(route, handle) {
     response.writeHead(200, {
       "Content-Type": 'text/plain'
     })
-    const content = route(handle, pathname)
-    response.write(content)
-    response.end()
+    route(handle, pathname, response)
   }
 
   http.createServer(onRequest).listen(8888)
