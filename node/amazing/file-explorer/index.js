d230a5d78a5f0fc89cc24827e49d202dfba8afe4
张伟峰 <zhang.weifeng@tslsmart.com>
2022-02-17 11:11:53 +0800

    feat: 增加输入响应处理函数

--- node/amazing/file-explorer/index.js
+++ node/amazing/file-explorer/index.js
@@ -3,6 +3,7 @@
  */
 
 var fs = require('fs')
+const {stdin, stdout} = process
 const e = require("express");
 fs.readdir(process.cwd(), function (err,files) {
   console.log(''); // 一个空行
@@ -13,9 +14,13 @@ fs.readdir(process.cwd(), function (err,files) {
 
   console.log(' Select which file or directory you want to see\n')
 
+  // 保留所有Stat对象
+  const stats = [];
+  // called for each file walked in the directory
   function file (i) {
     var filename = files[i];
     fs.stat(__dirname + '/' + filename, function (err, stat) {
+      stats[i] =stat;
       if (stat.isDirectory()) {
         console.log('   ' +i+'   \033[36m' + filename + '/\033[39m')
       } else {
@@ -24,16 +29,50 @@ fs.readdir(process.cwd(), function (err,files) {
 
       i++;
       if (i === files.length) {
-        console.log('')
-        process.stdout.write('   \033[33mEnter your choice: \033[39m');
-        // 等待用户输入
-        process.stdin.resume()
-        // 设置编码格式为utf8，这样就可以输入特殊字符
-        process.stdin.setEncoding('utf8')
+        read()
       } else {
         file(i)
       }
     })
+  }
+
+  // read user input when files are shown
+  function read() {
+    console.log('')
+    stdout.write('   \033[33mEnter your choice: \033[39m');
+    // 等待用户输入
+    stdin.resume()
+    // 设置编码格式为utf8，这样就可以输入特殊字符
+    stdin.setEncoding('utf8')
+
+    stdin.on("data", option)
+  }
+
+  // called with the option supplied by the user
+  function option(data) {
+    const filename = files[Number(data)]
+    if (!filename) {
+      stdout.write('   \033[31mEnter your choice: \033[39m');
+    } else {
+      // 将流暂停（回到默认状态）
+      stdin.pause();
+      if (stats[Number(data)].isDirectory()) {
+        fs.readdir(__dirname + '/' + filename, function (err, files) {
+          console.log('')
+          console.log('   (' + files.length + ' files)')
+          files.forEach(function (file) {
+            console.log('   -   ' + file)
+          })
+          console.log('')
+        })
+      } else {
+        // 指定编码
+        fs.readFile(__dirname+ '/' + filename, 'utf8', function (err,data) {
+          console.log('')
+          console.log('\033[90m' + data.replace(/(.*)g/, '   $1') + '\033[39m')
+        })
+      }
+    }
   }
 
   file(0)
