13c704b00a0cd84dc351b11c66bfb60b44410fa6
张伟峰 <zhang.weifeng@tslsmart.com>
2022-02-21 18:22:20 +0800

    feat: 增加错误处理和一些get的api

--- node/amazing/express-tweet/server.js
+++ node/amazing/express-tweet/server.js
@@ -7,11 +7,18 @@ const app = express()
 
 // 配置
 app.set('view engine', 'ejs')
-app.set('Views', __dirname + '/views')
-app.set('view options', {layout: false})
+app.set('views', process.cwd() + '/views')
+// 增加页面缓存 生产模式下开启
+// app.set('view cache', true)
 
 // 路由
 app.get('/', function (req, res) {
+  // 显示请求头信息，大小写不敏感
+  console.log(req.header('host'))
+  // 显示请求中的Accept头信息
+  console.log(req.accepts('html'))
+  // 检查Content-Type信息
+  console.log(req.is('text/html'))
   res.render('index')
 })
 app.get('/search', function (req, res, next) {
@@ -22,6 +29,12 @@ app.get('/search', function (req, res, next) {
       search: req.query.q
     })
   })
+})
+
+// 错误处理
+app.use(function (err, req, res, next) {
+  console.error(err.stack)
+  res.status(500).send('Something broke!')
 })
 
 // 监听
