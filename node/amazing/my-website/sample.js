ee89c1abab6738f3afbcb1aa2668630a31a85efc
张伟峰 <zhang.weifeng@tslsmart.com>
2022-02-21 16:07:14 +0800

    style: format

--- node/amazing/my-website/sample.js
+++ node/amazing/my-website/sample.js
@@ -6,7 +6,7 @@ const fs = require("fs");
 const formidable = require('formidable')
 const cookieParser = require('cookie-parser')
 // 创建服务
-const app =connect()
+const app = connect()
 // 记录请求情况
 // app.use(connect.logger('dev'))
 app.use(logger())
@@ -17,18 +17,18 @@ app.use(cookieParser())
 // 实现时间中间件
 app.use(time({time: 500}))
 // 快速响应
-app.use(function (req,res,next) {
+app.use(function (req, res, next) {
   if ('/a' === req.url) {
     res.writeHead(200)
     res.end('Fast!')
-  }else {
+  } else {
     next()
   }
 })
 // 慢速响应
 app.use(function (req, res, next) {
   if ('/b' === req.url) {
-    setTimeout(function (){
+    setTimeout(function () {
       res.writeHead(200)
       res.end('Slow!')
     }, 1000)
@@ -47,12 +47,12 @@ app.use(function (req, res, next) {
   }
 })
 
-app.use(function (req,res,next) {
-  if ('/form' === req.url ) {
+app.use(function (req, res, next) {
+  if ('/form' === req.url) {
     if ('GET' === req.method) {
       fs.createReadStream(__dirname + '/sample.html').pipe(res)
     } else if ('POST' === req.method) {
-      const form = formidable({ multiples: true })
+      const form = formidable({multiples: true})
       form.parse(req, (err, fields, files) => {
         if (err) {
           res.writeHead(err.httpCode || 400, {'Content-Type': 'text/plain'})
