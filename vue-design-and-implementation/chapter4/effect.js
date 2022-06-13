968115c8405b61552f505f40d4a4b1149ef02f15
张伟峰 <wulakaka@live.com>
2022-06-13 11:24:32 +0800

    feat(vue设计与实现): 增加分支切换功能

--- vue-design-and-implementation/chapter4/effect.js
+++ vue-design-and-implementation/chapter4/effect.js
@@ -1,27 +1,23 @@
 // 用一个全局变量存储被注册的副作用函数
 let activeEffect
-// effect 函数用于注册副作用函数
-function effect(fn) {
-  // 当调用 effect 注册副作用函数时，将副作用函数赋值给全局变量
-  activeEffect = fn
-  // 执行
-  fn()
-}
+
 // 存储副作用函数的桶
 const bucket = new WeakMap()
 
 // 原始数据
-const data = {text: 'hello world'}
+const data = {text: 'hello world', ok: true}
 // 对原始数据的代理
 const obj = new Proxy(data, {
 //  拦截读取操作
   get(target, p, receiver) {
+    console.log('get', p)
     track(target, p)
     // 返回属性值
     return target[p]
   },
   // 拦截设置操作
   set(target, p, value, receiver) {
+    console.log('set', p)
     // 设置属性值
     target[p] = value
     // 把副作用函数从桶里取出来并执行
@@ -29,7 +25,36 @@ const obj = new Proxy(data, {
   }
 })
 
-function track(target,key){
+// effect 函数用于注册副作用函数
+function effect(fn) {
+  const effectFn = () => {
+    // 调用 cleanup 函数完成清除工作
+    // 清除工作是针对于副作用函数的，而不是针对于对象的某个属性
+    cleanup(effectFn)
+    // 当 effectFn 执行时，将其设置为当前激活的副作用函数
+    activeEffect = effectFn
+    fn()
+  }
+  // activeEffect.deps 用来存储所有与该副作用函数相关联的依赖集合（哪个集合里包含了该副作用函数）
+  // 相当于存储了当前函数的父关系
+  effectFn.deps = []
+  // 执行副作用函数
+  effectFn()
+}
+
+function cleanup(effectFn) {
+  // 遍历 effectFn.deps 数组
+  for (let i = 0; i < effectFn.deps.length; i++) {
+    // deps 是依赖集合
+    const deps = effectFn.deps[i]
+    // 将 effectFn 从依赖集合中移除
+    deps.delete(effectFn)
+  }
+  // 最后需要重置 effectFn.deps 数组
+  effectFn.deps.length = 0
+}
+
+function track(target, key) {
   // 没有 activeEffect 直接 return
   if (!activeEffect) return
   // 根据 target 从桶中取得 depsMap, 它也是一个 Map 类型
@@ -45,22 +70,29 @@ function track(target,key){
   if (!deps) {
     depsMap.set(key, (deps = new Set()))
   }
-
+  // 把当前激活的副作用函数添加到依赖集合 deps 中
   deps.add(activeEffect)
+  // deps 就是一个与当前副作用函数存在联系的依赖结合
+  // 将其添加到 activeEffect.deps 数组中
+  activeEffect.deps.push(deps)
 }
 
-function trigger(target, key){
+function trigger(target, key) {
   const depsMap = bucket.get(target)
   if (!depsMap) return
   // 根据 key 取得所有的副作用函数
   const effects = depsMap.get(key)
+
+  // 避免无限循环
   // 执行副作用函数
-  effects && effects.forEach(fn =>fn())
+  const effectsToRun = new Set(effects)
+  effectsToRun.forEach(fn => fn())
 }
 
-// 执行副作用函数，触发读取
+// 执行副作用函数，触发读取，建立依赖关系
 effect(() => {
-  document.body.innerText = obj.text
+  console.log('effect')
+  document.body.innerText = obj.ok ? obj.text: 'not'
 })
 
 // 1 秒钟后修改响应式数据
@@ -68,6 +100,3 @@ setTimeout(() => {
   obj.text = 'hello vue3'
 }, 1000)
 
-setTimeout(() => {
-  obj.text2 = 'hello vue3 again'
-}, 3000)
