b081d1b494b4037c29700e17cc0e2a8291dd533d
张伟峰 <zhang.weifeng@tslsmart.com>
2022-08-09 17:08:50 +0800

    feat(vue2-pxtorem): 引入自定义主题包

--- vue2-pxtorem/babel.config.js
+++ vue2-pxtorem/babel.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   presets: ["@vue/cli-plugin-babel/preset"],
+  plugins: [
+    [
+      "component",
+      {
+        libraryName: "element-ui",
+        styleLibraryName: "~node_modules/element-theme-tsl-purple/lib",
+      },
+    ],
+  ],
 };
--- vue2-pxtorem/src/main.js
+++ vue2-pxtorem/src/main.js
@@ -1,22 +1,14 @@
 import Vue from "vue";
 import App from "./App.vue";
-import TslTable from '@tslsmart/bigscreen-table'
-import '@tslsmart/bigscreen-table/lib/tsl-table.css'
-Vue.use(TslTable);
+// import TslTable from "@tslsmart/bigscreen-table";
+// import "@tslsmart/bigscreen-table/lib/tsl-table.css";
+import { Button, Select } from "element-ui";
+// import "element-theme-tsl-purple";
+Vue.use(Button);
+Vue.use(Select);
+// Vue.use(TslTable);
 Vue.config.productionTip = false;
 
 new Vue({
   render: (h) => h(App),
 }).$mount("#app");
-function Foo(a) {
-  this.a = a;
-}
-Foo.prototype.bar = function () {
-  console.log(this.a);
-};
-
-function Sub() {}
-Sub.prototype = new Foo(4);
-const obj = new Sub();
-obj.bar();
-console.log(Foo === Foo.prototype.constructor);
